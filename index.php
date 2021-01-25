ecf693ecb8bebea01cf255af3c7ceeb7933dac64
ericyork <eyork@clarkson.edu>
2021-01-25 19:33:12 +0000

    adds support for displaying data

--- index.php
+++ index.php
@@ -7,19 +7,35 @@
   </head>
   <body>
     <div>
-      <h2>Have we DB?</h2>
+      <h2>Have We DB?</h2>
       <?php include "config.php" ?>
+      <hr />
     </div>
     <div>
-      <h2>Display some data</h2>
+      <h2>Display One Entry</h2>
       <?php
         $query = "SELECT make, model, year FROM cars";
         $result = $mysqli->query($query);
         /* associative array */
         $row = $result->fetch_array(MYSQLI_ASSOC);
         printf ("%d %s %s\n", $row["year"], $row["make"], $row["model"]);
-        echo "<p>My first car is a " . $row["year"] . " " . $row["make"] . " " . $row["model"] . ". </p>";
+        echo "<p>My top pick is a " . $row["year"] . " " . $row["make"] . " " . $row["model"] . ". </p>";
       ?>
+      <hr />
+    </div>
+    <div>
+      <h2>Display All Entries</h2>
+      <ul>
+        <?php
+          $con = new mysqli("localhost", $dbuser, $dbpass, $dbname);
+          $call = "SELECT make, model, year FROM cars";
+          $answer = $con->query($call);
+          while ($line = $answer->fetch_array(MYSQLI_ASSOC)) {
+            echo "<li><strong>" . $line["make"] . " " . $line["model"] . "</strong> (" . $line["year"] . ")" . ". </li>";
+          }
+        ?>
+      </ul>
+      <hr />
     </div>
   </body>
 </html>
